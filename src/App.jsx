23a245432fa811d4951f131d7a02516cb0f8572f
Suraj SK <surajsk2002@gmail.com>
2022-05-14 21:45:28 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="app">
         <h1>QR-CODE GENERATOR</h1>
+        <input type="text" placeholder="https://www.example.com"></input>
+        <button>Prompt</button>
     </div>
   )
 }
